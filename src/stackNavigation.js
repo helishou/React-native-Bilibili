96fc45bfa85b581740b216f22ca2afe6e55feb66
helishou <41136716+helishou@users.noreply.github.com>
2021-04-11 02:34:39 +0800

    实现全屏,修改了些细节

---
+++ src/stackNavigation.js
@@ -0,0 +1,43 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React, {Component} from 'react';
+import {
+  Image,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  useWindowDimensions,
+  TouchableOpacity,
+} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import MaterialTopTabNavigator from './materialTopTabNavigator';
+
+import Search from './pages/search';
+
+const StackTab = createStackNavigator();
+export default function StackNavigator() {
+  return (
+    <StackTab.Navigator
+    // screenOptions={{headerShown:false}}
+    >
+      <StackTab.Screen
+        name="Bilibili"
+        component={MaterialTopTabNavigator}
+        options={{title: '哔哩哔哩', headerShown: false}}></StackTab.Screen>
+      <StackTab.Screen
+        name="Search"
+        component={Search}
+        options={{title: '搜索'}}></StackTab.Screen>
+      {/* <StackTab.Screen
+        name="earch"
+        component={Live}
+        options={{title: '搜索'}}></StackTab.Screen> */}
+      {/* <StackTab.Screen
+        name="VideoPlayDetail"
+        component={VideoPlayDetail}
+        options={{title: '视频播放'}}></StackTab.Screen> */}
+    </StackTab.Navigator>
+  );
+}
